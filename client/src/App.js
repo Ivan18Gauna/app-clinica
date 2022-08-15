69afb7d92b4e4118c05511954ccaef441644a87d
CristianmMst <cristianmmst@gmail.com>
2022-08-15 18:41:17 -0500

    connect to db

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
+import LandingPage from './components/landingPage/LandingPage';
+import Details from './components/detail/Details';
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from './components/home/Home'
-import LandingPage from './components/landingPage/LandingPage'
-import Details from './components/detail/Details'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div>
-        <Switch>
-        <Route exact  path="/" component={LandingPage} />
-          <Route path="/home" component={Home} />
-          <Route path='/details' component={Details}/>
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/home" element={<Home />} />
+				<Route path="/details" element={<Details />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
