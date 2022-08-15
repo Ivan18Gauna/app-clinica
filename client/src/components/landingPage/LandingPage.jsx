30c68fc98f4cf4ad89e4f60120236a1598e58702
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-08-15 18:08:11 -0300

    Boton LandingPage

--- client/src/components/landingPage/LandingPage.jsx
+++ client/src/components/landingPage/LandingPage.jsx
@@ -3,6 +3,11 @@ import React from "react";
 
 export default function LandingPage (){
     return(
-        <h1>Esto es el LandingPage</h1>
+        <div className="landing">
+            <h1 >App Clinica</h1>
+            <Link to = "/home">
+                <button>Ingresar</button>
+            </Link>
+        </div>
     )
 }
