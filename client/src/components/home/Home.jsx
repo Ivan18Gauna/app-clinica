36f7060783851d753815e4c1672f8e7832fd6a9a
CristianmMst <cristianmmst@gmail.com>
2022-08-16 18:55:43 -0500

    Archivo:Home

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -6,7 +6,6 @@ import Cards from '../cards/Cards';
 export default function Home() {
 	const dispatch = useDispatch();
 	const doctors = useSelector((state) => state.doctors);
-	console.log(doctors);
 	useEffect(() => {
 		dispatch(get_Doctors());
 	}, [dispatch]);
@@ -14,6 +13,24 @@ export default function Home() {
 	return (
 		<div>
 			<h1>Esto es el Home</h1>
+			<form>
+				<input type={'text'} placeholder={'Por nombre...'} />
+				<select defaultValue={'especialidad'}>
+					<option value="especialidad" hidden>
+						Especialidad
+					</option>
+					<option value="actividad">Actividad Fisica</option>
+					<option value="alergia">Alergia</option>
+				</select>
+				<select defaultValue={'ubicacion'}>
+					<option value="ubicacion" hidden>
+						Ubicacion
+					</option>
+					<option value="argentina">Argentina</option>
+					<option value="canada">Canada</option>
+				</select>
+				<button type={'submit'}>Buscar</button>
+			</form>
 			<Cards doctors={doctors} />
 		</div>
 	);
