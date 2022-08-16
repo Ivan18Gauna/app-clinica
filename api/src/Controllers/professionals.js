f2ecf7db98399d6e4fe2f4657bc1737ba4e1a83f
jcariajano <juliocariajano@gmail.com>
2022-08-16 14:00:57 -0500

    Sicronizacion dev-back-main  para deploy del backend

--- api/src/Controllers/professionals.js
+++ api/src/Controllers/professionals.js
@@ -2,24 +2,6 @@ const Sequelize = require("sequelize");
 const { default: axios } = require("axios");
 const { Professionals, Specialties } = require("../db");
 
-// const getProfInfo = async() => {
-//     const dbProf = await Professionals.findAll();
-//     if (!dbProf.length) {
-//         const apiProf = await axios.get('https://historia-clinica-31f40-default-rtdb.firebaseio.com/results.json').data;
-//         await apiProf.map(async(e) => {
-//             await Professionals.findOrCreate({
-//                 where: {
-//                     name: e.name
-//                 }
-//             })
-//         })
-//         res.status(200).send(dbProf)
-//     }else {
-//         res.status(200).send(dbProf)
-//     }
-//     return res.status(400).send('no data')
-// }
-// console.log(getProfInfo)
 
 const getInfoApi = async(req, res) => {
     const dbProf = await Professionals.findAll()
@@ -73,7 +55,7 @@ const getProfById = async(req, res) => {
     })
     return res
 };
-
+//
 const getAllProfessionals = async ()=>{
     const infoApi = await getInfoApi();
     const dbInfo = await getDbInfo();
