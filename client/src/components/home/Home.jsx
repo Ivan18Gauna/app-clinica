886af153b68e0dd82c4fecb74acde22e863c0305
CristianmMst <cristianmmst@gmail.com>
2022-08-16 17:17:08 -0500

    Archivo: Home

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,24 +1,20 @@
-import {React, useState, useEffect} from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { get_Doctors } from "../../redux/actions";
-import Cards from "../cards/Cards";
+import { React, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { get_Doctors } from '../../redux/actions';
+import Cards from '../cards/Cards';
 
+export default function Home() {
+	const dispatch = useDispatch();
+	const doctors = useSelector((state) => state.doctors);
+	console.log(doctors);
+	useEffect(() => {
+		dispatch(get_Doctors());
+	}, [dispatch]);
 
-export default function Home (){
-
-    const dispatch = useDispatch();
-    const doctors = useSelector(state => state.doctors);
-console.log(doctors)
-    useEffect(()=>{
-        dispatch(get_Doctors());
-    }, [dispatch]);
-
-    return(
-        <div>
-        <h1>Esto es el Home</h1>
-        <Cards
-        doctors={doctors} 
-        />
-       </div>
-    )
+	return (
+		<div>
+			<h1>Esto es el Home</h1>
+			<Cards doctors={doctors} />
+		</div>
+	);
 }
