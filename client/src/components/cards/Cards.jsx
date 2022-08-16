308c61a35bd60dcd3a1d0b76f2a67a88111c8980
nicotacchia <nico.tacchia@hotmail.com.ar>
2022-08-16 09:50:29 -0300

    card

--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -3,9 +3,21 @@ import Card from "../card/Card";
 
 
 export default function Cards(){
-    return(
-        <div>
-            
-        </div>
+
+    const allProfessional = [{name: "Dr Ruperto",specialties: "Cirujano"},
+    {name: "Dr Salamanca",specialties: "Cardiologo"},
+    {name: "Dr Ahorro",specialties: "Clinico"},
+    {name: "Dra Misil",specialties: "Dermatologo"},
+]
+    return( <div>
+        {allProfessional.map((e)=>{
+            return(
+                <Card
+                name = {e.name}
+                specialties = {e.specialties}
+                />
+            )
+        })}
+       </div>
     )
 }
