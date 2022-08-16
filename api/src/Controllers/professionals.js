82aad2567de99104f9c77c419a169babf89f25f0
Felipe-Mina <88908796+Felipe-Mina@users.noreply.github.com>
2022-08-16 18:38:37 -0300

    Specialities create

--- api/src/Controllers/professionals.js
+++ api/src/Controllers/professionals.js
@@ -2,25 +2,6 @@ const Sequelize = require("sequelize");
 const { default: axios } = require("axios");
 const { Professionals, Specialties } = require("../db");
 
-// const getProfInfo = async() => {
-//     const dbProf = await Professionals.findAll();
-//     if (!dbProf.length) {
-//         const apiProf = await axios.get('https://historia-clinica-31f40-default-rtdb.firebaseio.com/results.json').data;
-//         await apiProf.map(async(e) => {
-//             await Professionals.findOrCreate({
-//                 where: {
-//                     name: e.name
-//                 }
-//             })
-//         })
-//         res.status(200).send(dbProf)
-//     }else {
-//         res.status(200).send(dbProf)
-//     }
-//     return res.status(400).send('no data')
-// }
-// console.log(getProfInfo)
-
 const getInfoApi = async(req, res) => {
     const dbProf = await Professionals.findAll()
     if (!dbProf.length) {
@@ -42,6 +23,13 @@ const getInfoApi = async(req, res) => {
                 }
             })
         })
+        prof.forEach((e) => {
+            Specialties.findOrCreate({
+                where: {
+                    name: e.specialty
+                }
+            })
+        })
         return res.status(200).send(await Professionals.findAll())
     }
     res.status(200).send(await Professionals.findAll())
@@ -56,35 +44,6 @@ const getProfById = async(req, res) => {
     })
     res.status(200).send(dbProfId)
 }
-
-
-/* const getDbInfo= async ()=>{
-    const profesionalsDb= await Professionals.findAll({include:[{model:Specialties}]});
-    const res =  profesionalsDb.map(e=>{
-        return {
-            id: e.id,
-            name:e.name, 
-            license: e.license,
-            birth: e.birth, 
-            phone: e.phone,
-            mail: e.mail, 
-            domicile: e.domicile          
-        }
-    })
-    return res
-};
-
-const getAllProfessionals = async ()=>{
-    const infoApi = await getInfoApi();
-    const dbInfo = await getDbInfo();
-    const allInfoApiDb = await infoApi.concat(dbInfo);
-    // console.log(allInfoApiDb)
-    return allInfoApiDb;
-}; */
-
-
-
-
 
 module.exports = {
     getInfoApi,
