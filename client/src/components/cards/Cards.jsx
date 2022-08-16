f7659de52b1601e39b5c01c784d8039952032340
nicotacchia <nico.tacchia@hotmail.com.ar>
2022-08-16 12:38:55 -0300

    CardComplete

--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import Card from "../card/Card";
 
 
-export default function Cards(){
+export default function Cards({doctors}){
 
-    const allProfessional = [{name: "Dr Ruperto",specialties: "Cirujano"},
+ /*    const allProfessional = [{name: "Dr Ruperto",specialties: "Cirujano"},
     {name: "Dr Salamanca",specialties: "Cardiologo"},
     {name: "Dr Ahorro",specialties: "Clinico"},
     {name: "Dra Misil",specialties: "Dermatologo"},
-]
+] */
     return( <div>
-        {allProfessional.map((e)=>{
+        {doctors.map((e)=>{
             return(
                 <Card
                 name = {e.name}
-                specialties = {e.specialties}
+                
+ /*                specialties = {e.specialties} */
                 />
             )
         })}
--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,16 +1,14 @@
-
-
 import {React, useState, useEffect} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { get_Doctors } from "../../redux/actions";
-
+import Cards from "../cards/Cards";
 
 
 export default function Home (){
 
     const dispatch = useDispatch();
     const doctors = useSelector(state => state.doctors);
-
+console.log(doctors)
     useEffect(()=>{
         dispatch(get_Doctors());
     }, [dispatch]);
@@ -18,6 +16,9 @@ export default function Home (){
     return(
         <div>
         <h1>Esto es el Home</h1>
+        <Cards
+        doctors={doctors} 
+        />
        </div>
     )
 }
