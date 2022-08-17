5020f5332d97a637b1bc0029d63106b8a55c0ff9
nicotacchia <nico.tacchia@hotmail.com.ar>
2022-08-17 08:45:33 -0300

    ok

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -4,7 +4,7 @@ export default function Card({name}) {
     return (
         <div>
             <div>
-                <img src="" alt="img not found" />
+                <img src="https://c8.alamy.com/compes/2dtw8p3/estetoscopio-vector-salud-medico-logotipo-medico-2dtw8p3.jpg" alt="img not found" width="150px" height="200" />
             </div>
             <div>
                 <h3>{name}</h3>
