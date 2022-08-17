fc893ea712fe55db4fa809708bd7fb3aecce4323
Sebastian-Basmachi <101119494+Sebastian-Basmachi@users.noreply.github.com>
2022-08-17 20:08:51 -0300

    about

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -2,16 +2,19 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {Link} from "react-router-dom";
+import "./navBar.css"
 
 function navBarEdit() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
+      <div className='Div-Salud'>
         <Navbar.Brand as={Link} to="/home">+Salud</Navbar.Brand>
+      </div>
+      <Container>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Nosotros</Nav.Link>
+            <Nav.Link as={Link} to="/about">Nosotros</Nav.Link>
             <Nav.Link href="#pricing">Precios</Nav.Link>
           </Nav>
           <Nav>
