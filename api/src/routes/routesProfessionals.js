826897c68a96c73972f2f27dfe0b9d3adff864f1
jcariajano <juliocariajano@gmail.com>
2022-08-17 13:47:15 -0500

    Actualizaciones filter

--- api/src/routes/routesProfessionals.js
+++ api/src/routes/routesProfessionals.js
@@ -4,7 +4,6 @@ const express = require('express')
 const router =express.Router();
 
 const {Professionals, Specialties} = require ('../db')
-//.
 router.get('/', getInfoApi)
 
 router.get('/:id', getProfById)
