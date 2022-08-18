8506b6d4353cfc58b17d0be337d928c06932f8c5
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-08-18 13:24:54 -0300

    inicio dia 18-8

--- api/src/routes/routesProfessionals.js
+++ api/src/routes/routesProfessionals.js
@@ -1,5 +1,5 @@
 const axios = require ('axios')
-const {getAllProfessionals, getInfoApi, getProfById, getProfByName, getFilterByCity, postProfessionals}= require ('../Controllers/professionals')
+const {putProfessionals, getInfoApi, getProfById, getProfByName, getFilterByCity, postProfessionals}= require ('../Controllers/professionals')
 const express = require('express')
 const router =express.Router();
 
