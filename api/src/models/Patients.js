576aad2d694f91fb379618a6b133f7442ea25ded
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-08-19 12:28:12 -0300

    paranoid: true

--- api/src/models/Patients.js
+++ api/src/models/Patients.js
@@ -47,5 +47,6 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false
   },
+  paranoid: true,
   });
 };
--- api/src/models/professionals.js
+++ api/src/models/professionals.js
@@ -46,5 +46,6 @@ module.exports = (sequelize) => {
         type: DataTypes.STRING,
         allowNull: false
     },
+    paranoid: true,
   });
 };
