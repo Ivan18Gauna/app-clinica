c1b11f8cc25b187f7f4001f9766800f6b06480b9
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-08-22 20:27:42 -0300

    Modelo, post, get y ruta Healthdata

--- api/src/db.js
+++ api/src/db.js
@@ -54,7 +54,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Professionals, Specialties, Patients, ObrasSociales, HistoriaClinica } = sequelize.models;
+const { Professionals, Specialties, Patients, ObrasSociales, HistoriaClinica, HealthData } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
--- api/src/models/HealthData.js
+++ api/src/models/HealthData.js
@@ -1,9 +1,15 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUIDV4 } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('healthData', {
+    id: {
+        type:DataTypes.STRING,
+          defaultValue: UUIDV4,
+          primaryKey: true,
+          allowNull: false,
+        },  
     blood: {
       type: DataTypes.STRING,
       allowNull: false,
