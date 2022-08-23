6fe60ac1ed11bdca7653cfc727e97226324441f9
marianettimatias <marianettimatiass@gmail.com>
2022-08-22 22:55:44 -0300

    Formulario healthdata sin terminar

--- client/src/components/formProfessionals/FormProfesionals.jsx
+++ client/src/components/formProfessionals/FormProfesionals.jsx
@@ -186,7 +186,7 @@ export default function RegisterDoctor() {
 						}
 
 					</select>
-					{input.province === '' ? <p>*</p> : ''}
+					{input.specialities === '' ? <p>*</p> : ''}
 				</div>
 				<div>
 					<ul>
