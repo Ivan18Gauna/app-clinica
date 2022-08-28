480ed606c32b642e19a9454e0926a1eddc9a6b6b
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2022-08-28 13:28:17 -0300

    App

--- client/src/App.js
+++ client/src/App.js
@@ -17,12 +17,8 @@ import ClinicHistory from './components/clinicHistory/ClinicHistory';
 import UserProfile from './components/userProfile/UserProfile';
 import Auth0 from './components/auth0/Auth0';
 import HomeProfessional from './components/homeProfessionals/HomeProfessionals';
-<<<<<<< HEAD
 import Calendar from './components/calendar/Calendar'
-
-=======
 import HomePatients from './components/homePatients/HomePatients';
->>>>>>> dev-front
 function App() {
   return (
     <BrowserRouter>
