8c240515c2954ab4418a4234be034ffe709ca796
Sebastian-Basmachi <101119494+Sebastian-Basmachi@users.noreply.github.com>
2022-08-30 23:26:07 -0300

    algunos cambios

--- client/src/components/userProfile/UserProfile.jsx
+++ client/src/components/userProfile/UserProfile.jsx
@@ -3,27 +3,27 @@ import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { getObrasSociales, getUserDetail } from "../../redux/actions";
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 import Cookies from "universal-cookie";
-import PatientProfile from '../patientsProfile/PatientsProfile'
+import PatientProfile from '../patientsProfile/PatientsProfile';
+import Loading from "../loading/Loading";
+import '../login/Login.module.css';
+import Button from "react-bootstrap/esm/Button";
+import styles from '../patientsProfile/PatientsProfile.module.css';
 
 export default function UserProfile() {
+
   const cookie = new Cookies()
   const history = useHistory()
   const dispatch = useDispatch();
-  
-  useEffect(() => {
-    
-    console.log('efect')
-    dispatch(getObrasSociales());
-    dispatch(getUserDetail(cookie.get('email')));
-  }, [dispatch]);
-  
   const state = useSelector((state) => state.user)
   const obras = useSelector((state) => state.os)
-
-  console.log("state",state)
-
+  
+  useEffect(() => {
+    dispatch(getObrasSociales());
+    dispatch(getUserDetail(cookie.get('email')));
+  }, []);
+  
   function logoutCookies (){
     cookie.remove('email',{path:'/'})
     cookie.remove('userEmail',{path:'/'})
@@ -33,14 +33,14 @@ export default function UserProfile() {
   return (
     <div>
       {state && state.document ? (
-       <div>
-         <PatientProfile globalUser={state} obras={obras}/>
-         <button onClick={logoutCookies}>cerrar local</button>
-       </div>
-      ) : (
         <div>
-        <h1>loading</h1>
-        <button onClick={logoutCookies}>cerrar local</button>
+          <PatientProfile globalUser={state} obras={obras}/>
+          <Button className={styles.button} onClick={logoutCookies}>Cerrar sesion</Button>
+        </div>
+      ) : (
+        <div className="loading-login">
+          <Loading />
+          <button onClick={logoutCookies}>cerrar local</button>
         </div>
       )}
     </div>
