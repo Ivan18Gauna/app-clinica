a1be4738cafa77bb71e02dcbd75bb477fd173e9b
Cristian Mora <cristianmmst@gmail.com>
2022-08-31 22:43:48 -0500

    ESTILOS HOME

--- client/src/components/portada/Portada.jsx
+++ client/src/components/portada/Portada.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import img from '../../Icons/Doctors.svg'
-import "./Portada.css"
+import React from 'react';
+import img from '../../Icons/Doctors.svg';
+import './Portada.css';
 
 export default function Portada() {
-    return (
-        <div className="portada">
-            <p>Bienvenidos a +Salud</p>
-            <img src={img} alt="img not found" />
-        </div>
-    )
+	return (
+		<div className="portada">
+			<img src={img} alt="img not found" />
+		</div>
+	);
 }
