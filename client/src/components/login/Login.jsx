cbaaa84bb93940370a7066e64036aaf241d6a399
Cristian Mora <cristianmmst@gmail.com>
2022-09-02 21:18:35 -0500

    login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,46 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import FormControl from '@mui/material/FormControl';
-import FormHelperText from '@mui/material/FormHelperText';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import PersonIcon from '@mui/icons-material/Person';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputAdornment from '@mui/material/InputAdornment';
-import IconButton from '@mui/material/IconButton';
-import InputLabel from '@mui/material/InputLabel';
-import Loading from '../loading/Loading';
-import { Link, useHistory } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getPatients, getUserDetail } from '../../redux/actions';
-import google from '../../Icons/google.svg';
-import styles from './Login.module.css';
-import stylesForm from '../formPatients/FormPatients.module.css';
-import Cookies from 'universal-cookie';
-import { Alert } from '@mui/material';
-import '../auth0/Auth0';
+import React, { useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import FormControl from "@mui/material/FormControl";
+import FormHelperText from "@mui/material/FormHelperText";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import PersonIcon from "@mui/icons-material/Person";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import InputLabel from "@mui/material/InputLabel";
+import Loading from "../loading/Loading";
+import { Link, useHistory } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPatients, getUserDetail, get_Doctors } from "../../redux/actions";
+import google from "../../Icons/google.svg";
+import styles from "./Login.module.css";
+import stylesForm from "../formPatients/FormPatients.module.css";
+import Cookies from "universal-cookie";
+import { Alert } from "@mui/material";
+import "../auth0/Auth0";
 
 const schema = yup
-	.object({
-		email: yup
-			.string()
-			.email('Ingresa un correo valido')
-			.required('Este campo es requerido'),
-		password: yup.string().required('Este campo es requerido')
-	})
-	.required();
+  .object({
+    email: yup
+      .string()
+      .email("Ingresa un correo valido")
+      .required("Este campo es requerido"),
+    password: yup.string().required("Este campo es requerido")
+  })
+  .required();
 
 export default function Login() {
   const cookies = new Cookies();
   const history = useHistory();
-  const globalUser = useSelector((state) => state.user);
+  const globalUser = useSelector(state => state.user);
   const dispatch = useDispatch();
   const { loginWithPopup, isAuthenticated, logout } = useAuth0();
   const {
@@ -48,53 +48,64 @@ export default function Login() {
     getValues,
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors }
   } = useForm({
     mode: "onBlur",
     resolver: yupResolver(schema),
     defaultValues: {
       email: "",
       password: "",
-      showPassword: false,
-    },
+      showPassword: false
+    }
   });
-  const values = getValues();
+  const [errorsExiste, setErrorsExiste] = useState({
+    email: "",
+    password: ""
+  });
 
-  const patients = useSelector((state) => state.patients);
-  const doctor = useSelector((state) => state.doctors);
+  const values = getValues();
+  const patients = useSelector(state => state.patients);
+  const doctor = useSelector(state => state.doctors);
   const [email, setEmail] = useState({
-    email:'',
-    password:''
+    email: "",
+    password: ""
   });
   function handleInput(e) {
     setEmail({
       ...email,
-      [e.target.name]: e.target.value,
+      [e.target.name]: e.target.value
     });
   }
-  console.log("email",email.email)
+  console.log("email", email.email);
   const allUSer = patients.concat(doctor);
 
   var filter;
   if (email.email) {
-    filter = allUSer.filter((el) => el.mail === email.email);
-    console.log("state",email.email)
+    filter = allUSer.filter(el => el.mail === email.email);
+    console.log("state", email.email);
   }
-  
-  const submitForm = (data) => {
+
+  const submitForm = data => {
     if (filter.length > 0) {
       if (data.password === filter[0].password) {
         dispatch(getUserDetail(data.email));
         cookies.set("email", `${data.email}`, { patch: "/" });
         history.push("/home");
       } else {
-        alert("la contraseña es incorrecta");
+        setErrorsExiste({
+          ...errorsExiste,
+          password: "La contraseña es incorrecta"
+        });
       }
     } else {
-      alert("El correo ingresado es incorrecto");
+      setErrorsExiste({
+        ...errorsExiste,
+        email: "El correo ingresado no existe"
+      });
     }
   };
-  const handleMouseDownPassword = (event) => {
+  console.log(errors);
+  const handleMouseDownPassword = event => {
     event.preventDefault();
   };
 
@@ -114,13 +125,13 @@ export default function Login() {
             <Row className={styles.formGroup} lg={1}>
               <Col className={styles.col} lg={9}>
                 <FormControl
-                onChange={(e)=>handleInput(e)}
-                name='email'
-                  error={errors.email}
+                  onChange={e => handleInput(e)}
+                  name="email"
+                  error={errors.email || errorsExiste.email}
                   className={styles.input}
                   variant="outlined"
                 >
-                  <InputLabel htmlFor="outlined-adornment-password" >
+                  <InputLabel htmlFor="outlined-adornment-password">
                     Correo electronico
                   </InputLabel>
                   <OutlinedInput
@@ -128,22 +139,23 @@ export default function Login() {
                     label="Correo electronico"
                     {...register("email")}
                     endAdornment={
-                        
                       <InputAdornment position="end">
                         <PersonIcon />
                       </InputAdornment>
                     }
                   />
-                  {errors.email && (
+                  {errors.email ? (
                     <FormHelperText>{errors.email.message}</FormHelperText>
-                  )}
+                  ) : errorsExiste.email ? (
+                    <FormHelperText>{errorsExiste.email}</FormHelperText>
+                  ) : null}
                 </FormControl>
               </Col>
               <Col className={styles.col} lg={9}>
                 <FormControl
-                onChange={(e)=>handleInput(e)}
-                name='password'
-                  error={errors.password}
+                  onChange={e => handleInput(e)}
+                  name="password"
+                  error={errors.password || errorsExiste.password}
                   className={styles.input}
                   variant="outlined"
                 >
@@ -162,7 +174,7 @@ export default function Login() {
                           onClick={() => {
                             let values = getValues("showPassword");
                             setValue("showPassword", !values, {
-                              shouldValidate: true,
+                              shouldValidate: true
                             });
                           }}
                           onMouseDown={handleMouseDownPassword}
@@ -177,9 +189,11 @@ export default function Login() {
                       </InputAdornment>
                     }
                   />
-                  {errors.password && (
+                  {errors.password ? (
                     <FormHelperText>{errors.password.message}</FormHelperText>
-                  )}
+                  ) : errorsExiste.password ? (
+                    <FormHelperText>{errorsExiste.password}</FormHelperText>
+                  ) : null}
                 </FormControl>
               </Col>
               <Col className={styles.col} lg={9}>
