138906b4a87cb8f6eeb9c40f7966b578ef4c4946
marianettimatias <marianettimatiass@gmail.com>
2022-09-03 11:59:43 -0300

    act

--- api/src/routes/routersMailer.js
+++ api/src/routes/routersMailer.js
@@ -3,26 +3,30 @@ var express = require('express')
 const router = express.Router()
 
 
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: "appclinicahenry@gmail.com",
+        pass: "Clinica123456"
+    },
+    port: 465,
+    host: 'smtp.gmail.com',
+    secure: true,
+});
+
 router.post("/send-email", (req, res) => {
-    var transporter = nodemailer.createTransport({
-        host: 'gmail.com',
-        auth: {
-            user: "appclinicahenry@gmail.com",
-            pass: "Clinica123456"
-        }
-    })
     var mailOptions = {
         from: "appclinicahenry@gmail.com",
         to: "marianettimatiass@gmail.com",
-        subjet: "Biemvenido a la app mas salud",
+        subjet: "Bienvenido a la app mas salud",
         text: "Hola Julio como estas? no solo hago malos formularios"
     };
-    let inform = transporter.sendMail(mailOptions, (error, info) => {
+    transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             res.status(500).send(error.message);
         } else {
             console.log("Email enviado")
-            res.status(200).send(inform)
+            res.status(200).send(info)
         }
     });
 });
