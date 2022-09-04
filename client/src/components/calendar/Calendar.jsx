63174b445516a7b1bb820007f167ee0026f34d14
Cristian Mora <cristianmmst@gmail.com>
2022-09-04 18:07:58 -0500

    action

--- client/src/components/calendar/Calendar.jsx
+++ client/src/components/calendar/Calendar.jsx
@@ -1,58 +1,59 @@
- import React, { useState } from "react";
- import { DatePicker, TimePicker } from "@material-ui/pickers";
- import "./Calendar.css";
- import { useDispatch, useSelector } from "react-redux";
- import { getUserDetail, newTurno } from "../../redux/actions";
+import React, { useState } from "react";
+import { DatePicker, TimePicker } from "@material-ui/pickers";
+import "./Calendar.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserDetail, newTurno, postTurnoMail } from "../../redux/actions";
 import { useHistory, useLocation } from "react-router-dom";
 import Cookies from "universal-cookie";
 import { useEffect } from "react";
 
- const Calendar = () => {
-    const cookies = new Cookies();
-    const history = useHistory();
-	const location = useLocation();
-	const professional = location.state;
-    const dispatch = useDispatch();
+const Calendar = () => {
+  const cookies = new Cookies();
+  const history = useHistory();
+  const location = useLocation();
+  const professional = location.state;
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getUserDetail(cookies.get('userEmail')))
-    }, [])
+  useEffect(() => {
+    dispatch(getUserDetail(cookies.get("userEmail")));
+  }, []);
 
-    const user = useSelector((state) => state.user)
+  const user = useSelector(state => state.user);
 
-   const [fecha, setFecha] = useState(new Date());
-   const onChangeSetFecha = (e) => {
-     setFecha(e);
-   };
+  const [fecha, setFecha] = useState(new Date());
+  const onChangeSetFecha = e => {
+    setFecha(e);
+  };
 
-   const arr = fecha.toString().split(' ')
-   const payload = {
- 	   date: arr[0] + ' ' + arr[2] + ' ' + arr[1] + ' ' + arr[3] + ' ',
- 	   time: arr[4].split(':')[0] + ':00',
- 	   professional: professional,
- 	   patient: user.id
-   }
+  const arr = fecha.toString().split(" ");
+  const payload = {
+    date: arr[0] + " " + arr[2] + " " + arr[1] + " " + arr[3] + " ",
+    time: arr[4].split(":")[0] + ":00",
+    professional: professional,
+    patient: user.id
+  };
 
-   const handleSubmit = (e) => {
- 	e.preventDefault(e)
- 	dispatch(newTurno(payload))
-   }
+  const handleSubmit = e => {
+    e.preventDefault(e);
+    dispatch(newTurno(payload));
+    dispatch(postTurnoMail(payload))
+  };
 
-   return (
- 	<div>
- 		<div className="contenedor">
- 		<div className="grupo">
- 			<label>Fecha</label>
- 			<DatePicker value={fecha} onChange={onChangeSetFecha} />
- 		</div>
- 		<div className="grupo">
- 			<label>Hora</label>
- 			<TimePicker value={fecha} onChange={onChangeSetFecha} />
- 		</div>
- 		</div>
- 		<button onClick={handleSubmit}>Solicitar turno</button>
- 	</div>
-   );
- };
+  return (
+    <div>
+      <div className="contenedor">
+        <div className="grupo">
+          <label>Fecha</label>
+          <DatePicker value={fecha} onChange={onChangeSetFecha} />
+        </div>
+        <div className="grupo">
+          <label>Hora</label>
+          <TimePicker value={fecha} onChange={onChangeSetFecha} />
+        </div>
+      </div>
+      <button onClick={handleSubmit}>Solicitar turno</button>
+    </div>
+  );
+};
 
- export default Calendar;
+export default Calendar;
